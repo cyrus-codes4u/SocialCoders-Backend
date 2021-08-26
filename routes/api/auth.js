6f169ed70e42dff48bc670e2e4dd44dde7d16678
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-26 15:26:47 -0400

    Use middleware in authorization to get user info

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+
+const User = require('../../models/User')
 
 // @route  GET api/auth
 // @desc   Test Route
-// @access Publis
-router.get('/', (req, res) => res.send('Auth router response'))
+// @access Protected
+router.get('/', auth, (req, res) => {
+  try {
+    //get user info from db using decoded token that contains the id excluding password
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
 
 module.exports = router
