0c4b03799adf3b3020511bcefb3be418f2d9d61f
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-26 14:48:18 -0400

    Added validation middleware to register a user

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const connectDB = require('./config/db')
 //Connect to database
 connectDB()
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+//Parse body of request
+app.use(express.json({ extended: false }))
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
