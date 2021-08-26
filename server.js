2bc1f3aba96cd7ce5374cc1cdc84323b3da2bd32
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-26 14:11:55 -0400

    Connection to MongoDB Atlas database

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/db')
+
+//Connect to database
+
+connectDB()
 
 const PORT = process.env.PORT || 5000
 
