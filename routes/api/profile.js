a29eb09a30b17ae45750842ca0d142da877d6e6e
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-27 11:55:26 -0400

    Added route to remove an experience item from user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,7 +157,65 @@ router('/user/:user_id').get(async (req, res) => {
 // @route  PUT api/profile/experience
 // @desc   Add profile experience
 // @access Private
-router('/experience').put(auth, async (req, res) => {})
+router('/experience').put(
+  [
+    auth,
+    [
+      check('company', 'Company is required').not().isEmpty(),
+      check('title', 'Title is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { from, to, company, title, location, description, current } =
+      req.body
+
+    const newExperience = {
+      from,
+      to,
+      title,
+      company,
+      location,
+      description,
+      current,
+    }
+    try {
+      const profile = Profile.findOne({ user: req.user.id })
+      profile.experience.unshift(newExperience)
+      await profile.save()
+      res.status(201).json(profile)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
+  }
+)
+
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete profile experience
+// @access Private
+router('/experience/:exp_id').delete(auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+
+    profile.experience.splice(removeIndex, 1)
+    await profile.save()
+
+    res.status(204).json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
 
 // @route  PUT api/profile/education
 // @desc   Add profile education
