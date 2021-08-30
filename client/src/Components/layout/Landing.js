a4cdca09c06fec97531d5182536c96cbcbbd011d
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-30 19:27:03 -0400

    Added content to Landing component

--- client/src/Components/layout/Landing.js
+++ client/src/Components/layout/Landing.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
-  return <div></div>
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>SocialCoders</h1>
+          <p className='lead'>
+            Create a profile, share posts and connect with other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Landing
