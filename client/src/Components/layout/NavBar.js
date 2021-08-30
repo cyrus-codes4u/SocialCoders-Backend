9e9d3f412fbe38b679d245e9a18db51d6cd7f94a
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-30 19:21:10 -0400

    Added content to NavBar component

--- client/src/Components/layout/NavBar.js
+++ client/src/Components/layout/NavBar.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 const NavBar = () => {
-  return <nav></nav>
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i /> SocialCoders
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Developers</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar
