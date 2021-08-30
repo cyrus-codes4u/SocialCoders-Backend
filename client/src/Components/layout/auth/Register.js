36270f5d2dbfb8d0cb338da3e2547b89e2c6e25b
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-30 19:46:35 -0400

    Added content to Register component (form to sign up to site)

--- client/src/Components/layout/auth/Register.js
+++ client/src/Components/layout/auth/Register.js
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, { userState } from 'react'
 
 const Register = () => {
-  return <div></div>
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const updateForm = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (formState.password !== formState.password2) {
+      console.log('Passwords do not match')
+    } else {
+      console.log(formState)
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user' />
+        Create Account
+      </p>
+      <form className='form' onSubmit={handleSubmit()}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            value={formState.name}
+            onChange={updateForm()}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={formState.email}
+            onChange={updateForm()}
+          />
+          <small className='form-text'>
+            This site uses gravatar so if you would like a preloaded avatar, use
+            your gravatar email.
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={formState.password}
+            onChange={updateForm()}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            value={formState.password2}
+            onChange={updateForm()}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        {' '}
+        Already have account? <Link to='/login'> Sign In</Link>
+      </p>
+    </React.Fragment>
+  )
 }
 
 export default Register
